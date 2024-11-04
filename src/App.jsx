7aea83811c540cf9fa3d448e7c9104a2f278fbc8
Criss <crissfx28@gmail.com>
2024-11-03 20:21:43 -0500

    create components destination and about

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import "./App.css";
+import About from "./components/About";
+import Destination from "./components/Destination";
 import Features from "./components/Features";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
@@ -13,6 +15,12 @@ function App() {
         </div>
         <div id="features">
           <Features />
+        </div>
+        <div id="destination">
+          <Destination />
+        </div>
+        <div id="about">
+          <About />
         </div>
       </main>
     </>
